021807efc772ca414950f8db264856d4271cb51d
VEXXXXXXXXXXXXXd <301076@sycamoreschools.org>
2026-09-02 14:16:17 -0400

    Clean up comments in bot_joiner.py
    Removed comments from the join process in bot_joiner.py.

--- bot_joiner.py
+++ bot_joiner.py
@@ -7,19 +7,14 @@ def join_gimkit(code, name):
             browser = p.chromium.launch(headless=True)
             page = browser.new_page()
 
-            # Open Gimkit join page
             page.goto("https://www.gimkit.com/join")
 
-            # Fill room code
             page.fill("input[name='code']", code)
             page.click("button[type='submit']")
-
             time.sleep(1)
 
-            # Fill name
             page.fill("input[name='name']", name)
             page.click("button[type='submit']")
-
             time.sleep(2)
 
             browser.close()
